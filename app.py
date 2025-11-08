eff18cfde271163399903fa1ff23050e523090fb
anhdnv <anhdnv.dev@gmail.com>
2025-11-08 13:20:02 +0700

    feat(common): Add vercel config

--- app.py
+++ app.py
@@ -61,6 +61,6 @@ def text_to_speech():
         print(f"Error generating TTS: {e}")
         return jsonify({"error": "Internal Server Error"}), 500
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=False)
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5001))
+#     app.run(host='0.0.0.0', port=port, debug=False)
